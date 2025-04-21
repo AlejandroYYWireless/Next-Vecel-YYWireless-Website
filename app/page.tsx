f3ef2f63b9cd60ce5734220a1cbcb3a702a31f51
Alejandro Bermudez <alejandro@example.com>
2025-04-21 11:21:41 -0500

    Changed example to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="bg-primary h-screen justify-center flex items-center text-4xl font-extrabold">
-      Example page
+      Home Page
     </div>
   );
 }
