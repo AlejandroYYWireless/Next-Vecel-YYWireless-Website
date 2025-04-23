f765fedbbb88567858a37a3a3a6387b500da9624
Alejandro Bermudez <alejandro@example.com>
2025-04-23 09:34:52 -0500

    typo

--- app/home/components/HomeSlogan.tsx
+++ app/home/components/HomeSlogan.tsx
@@ -1,7 +1,7 @@
 const HomeSlogan = () => {
   return (
     <h1 className="w-full  font-mokoto flex justify-center items-center font-bold text-5xl my-4">
-      Reporpose with Purpose
+      Repurpose to Reconnect
     </h1>
   );
 };
