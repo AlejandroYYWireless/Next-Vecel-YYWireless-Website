3268d208cf3eb3b702fefdead9c338027930dd35
Alejandro Bermudez <alejandro@example.com>
2025-04-29 10:45:40 -0500

    cleaned page

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,9 @@
-import { HomeBento } from "./home/components/HomeBento";
-import HomeCertificates from "./home/components/HomeCertificates";
-import HomeNumbers from "./home/components/HomeNumbers";
-import { HomePartners } from "./home/components/HomePartners";
-import HomeShinyButton from "./home/components/HomeShinyButton";
-import QualityAndLense from "./home/components/QualityAndLense";
 import YYGlobe from "./home/components/YYGlobe";
 
 export default function Home() {
   return (
     <div className="space-y-24 md:space-y-48 scrollbar-hide">
       <YYGlobe />
-      {/* <HomePartners />
-      <div>
-        <HomeNumbers />
-      </div>
-      <div className="container mx-auto">
-        <HomeBento />
-      </div>
-      <QualityAndLense />
-
-      <HomeCertificates /> */}
     </div>
   );
 }
