1c7c554e42bb8c2dd4ffc822ad4a6ef7e30201d7
Alejandro Bermudez <alejandro@example.com>
2025-04-29 09:15:09 -0500

    debugging

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,22 @@
 import { HomeBento } from "./home/components/HomeBento";
 import HomeCertificates from "./home/components/HomeCertificates";
-import HomeGlobe from "./home/components/HomeGlobe";
 import HomeNumbers from "./home/components/HomeNumbers";
-import { HomePartners } from "./home/components/HomePartners";
 import HomeShinyButton from "./home/components/HomeShinyButton";
-import QualityAndLense from "./home/components/QualityAndLense";
 
 export default function Home() {
   return (
     <div className="space-y-24 md:space-y-48 scrollbar-hide">
       <div className="relative">
-        <HomeGlobe />
         <div className="absolute left-1/2 bottom-[-110px]">
           <HomeShinyButton />
         </div>
       </div>
-      <HomePartners />
       <div>
         <HomeNumbers />
       </div>
       <div className="container mx-auto">
         <HomeBento />
       </div>
-      <QualityAndLense />
 
       <HomeCertificates />
     </div>
